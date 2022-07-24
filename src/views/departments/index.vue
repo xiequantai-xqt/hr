7c023a36fc28abc1e7798ae2069c62768f9f9dc0
carl <xiequantai@qq.com>
2022-07-24 23:02:20 +0800

    添加department模块的card

--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -1,9 +1,29 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <h2>
-        组织架构
-      </h2>
+      <el-card class="tree-card">
+        <!-- 用一个行列布局 -->
+        <el-row type="flex" justify="space-between" align="middle">
+          <el-col>
+            <span>黑马</span>
+          </el-col>
+          <el-col :span="4">
+            <el-row type="flex" justify="end">
+              <!-- 两个内容 -->
+              <el-col>负责人</el-col>
+              <el-col>
+                <!-- 下拉菜单，也可以添加属性trigger="click"，就是点击后显示 -->
+                <el-dropdown>
+                  <span>操作<i class="el-icon-arrow-drwn" /></span>
+                  <el-dropdown-menu slot="dropdown">
+                    <el-dropdown-item>添加子部门</el-dropdown-item>
+                  </el-dropdown-menu>
+                </el-dropdown>
+              </el-col>
+            </el-row>
+          </el-col>
+        </el-row>
+      </el-card>
     </div>
   </div>
 </template>
@@ -14,6 +34,10 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+.tree-card {
+  padding: 30px  140px;
+  font-size:14px;
+}
 </style>
+
